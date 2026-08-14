05a054ff9bfbb9544102f74bfa6dbbf42041e21c
wellanmini <sunkeeper316@gmail.com>
2026-08-14 09:15:41 +0800

    訂閱啟用

--- lib/constants/purchase_ids.dart
+++ lib/constants/purchase_ids.dart
@@ -1,5 +1,5 @@
 /// Store product IDs for PicGrid.
 abstract final class PurchaseIds {
-  /// TODO: Replace with the subscription product ID configured in Google Play.
-  static const adFreeSubscription = 'PIC_GRID_SUBSCRIPTION_PRODUCT_ID';
+  /// Monthly subscription that removes all ads while active.
+  static const adFreeSubscription = 'picgrids_subscription';
 }
