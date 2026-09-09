1648ee11775c1ab2ad06b2c0972c82e886d96e38
wellanmini <sunkeeper316@gmail.com>
2026-09-09 09:56:40 +0800

    sdj 1.0.6 上架

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,9 +17,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -75,6 +72,12 @@ android {
             dimension = "flavors"
             resValue("string", "app_name", "PicGrids")
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
 
